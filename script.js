168cd9530a1b19874a7c23a8382a54ea976b5933
Ytadpole <81>
2019-11-01 12:28:32 +0800

    nav catagory 完成

--- script.js
+++ script.js
@@ -6,13 +6,39 @@ window.onload= function(){
         console.log(navItemTitles[i].value);
         navItemTitles[i].addEventListener('mouseover', function(){
             navDropMenu[0].style.height = '201px';
-            navDropMenu[0].style.bordertop = '1px solid #e0e0e0';
-            navDropMenu[0].style.borderbottom = '1px solid #e0e0e0';
+            navDropMenu[0].style.borderTop = '1px solid #e0e0e0';
+            navDropMenu[0].style.borderBottom = '1px solid #e0e0e0';
         })
         navItemTitles[i].addEventListener('mouseout', function(){
             navDropMenu[0].style.height = '0';
-            navDropMenu[0].style.bordertop = '';
-            navDropMenu[0].style.borderbottom = '';
+            navDropMenu[0].style.borderTop = '0px solid #e0e0e0';
+            navDropMenu[0].style.borderBottom = '0px solid #e0e0e0';
         })
     }
+
+    var catagory = this.document.getElementById("catagory");
+    var catagoryUl = catagory.firstElementChild;
+    for(var i = 0; i < catagoryUl.childNodes.length; i++){
+        var li = catagoryUl.childNodes[i];
+        if("LI" == li.nodeName){
+            // li.addEventListener('mouseover', function(){
+            //     console.log(1);
+            //     var detail = catagoryUl.childNodes[i].firstElementChild;
+            //     detail.style.display = 'block';
+            // });
+            // li.addEventListener('mouseout', function(){
+
+            // });
+            li.addEventListener('mouseover', function(){
+                console.log(this.style);
+                var detail = this.firstElementChild;
+                detail.style.display = 'block';
+            });
+            li.addEventListener('mouseout', function(){
+                var detail = this.firstElementChild;
+                detail.style.display = 'none';
+            });
+        }
+    }
+
 }
